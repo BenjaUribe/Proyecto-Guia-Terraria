e7b71b8d6059850ba45ebe9e7f2af1318e6463e1
Benja Uribe <baua2004@gmail.com>
2024-05-03 11:04:55 -0400

    Actualizacion de la page Principal

--- app/page.js
+++ app/page.js
@@ -1,9 +1,25 @@
 "use client";
 
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
+
 export default function Home() {
   return (
     <main className="main">
-      
+    <h1>Guia de Terraria</h1>
+      <Tabs variant='soft-rounded' colorScheme='green'>
+        <TabList>
+          <Tab>Bosses</Tab>
+          <Tab>Eventos</Tab>
+        </TabList>
+        <TabPanels>
+            <TabPanel>
+              <p> AQUI TODO LO DE LOS BOSSES </p>
+            </TabPanel>
+            <TabPanel>
+              <p> AQUI TODO LO DE LOS EVENTOS </p>
+            </TabPanel>
+        </TabPanels>
+      </Tabs>
     </main>
   );
 }
