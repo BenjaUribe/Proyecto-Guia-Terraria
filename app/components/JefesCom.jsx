87d03084ca9b46c9878a07c46f02cd695e30baaa
Railer03 <kahler2017@gmail.com>
2024-05-14 22:54:24 -0400

    No deja commitear

--- app/components/JefesCom.jsx
+++ app/components/JefesCom.jsx
@@ -2,9 +2,16 @@ import jefes from "@/app/boss_list.json";
 
 function JefesCom({ sProp = "Rey Slime"}) {
   const currentBoss = jefes.datos.find((boss) => boss.bossName === sProp);
+  const vida = currentBoss.stats.life1;
+  const def = currentBoss.stats.armor;
+  const atq = currentBoss.stats.damage;
+
   return (
     <main>
       {currentBoss && (<img src={currentBoss.bossImage} alt={sProp} />)}
+      <div>Vida: {vida}</div>
+      <div>Defensa: {def}</div>
+      <div>Ataque: {atq}</div>
     </main>
   );
   }
